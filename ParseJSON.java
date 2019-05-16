c0f172545af75064c143c54b817a6bc4d25641f6
Olga <44566631+arnautolea@users.noreply.github.com>
2019-05-16 14:57:17 +0300

    Update ParseJSON.java

--- ParseJSON.java
+++ ParseJSON.java
@@ -20,60 +20,58 @@ public class ParseJSON {
 		// Convert object to JSONObject
 		JSONObject jsonObject = (JSONObject) obj;
 		System.out.println(jsonObject); //Output object as a string
-		// Create company's array
-		JSONArray company = (JSONArray) jsonObject.get("company");
-		// Loop company and get departments
-		for (int c = 0; c < company.size() ; c++) {
-			System.out.println("-------company-------");
-			JSONObject eachCom =  (JSONObject) company.get(c);
-			JSONArray department = (JSONArray) eachCom.get("department");
-			// Loop departments to get values for name, depId and create emplyee's array
-			for (int d = 0; d < department.size() ; d++) {
-				System.out.println("\n-----department-----");
-				JSONObject eachDep =  (JSONObject) department.get(d);
+		// Create company's object
+		JSONObject company = (JSONObject)jsonObject.get("company");
+		System.out.println("-------company-------");
+		// Create department's array
+		JSONArray department = (JSONArray) company.get("department");
+		// Loop departments to get values for name, depId and create emplyee's array
+		for (int d = 0; d < department.size() ; d++) {
+			System.out.println("\n-----department-----");
+			JSONObject dep =  (JSONObject) department.get(d);
 
-				String name = (String) eachDep.get("name");
-				System.out.println("Name: " + name);
+			String name = (String) dep.get("name");
+			System.out.println("Name: " + name);
 
-				String depId = (String) eachDep.get("depId");
-				System.out.println("depId: " + depId);
+			String depId = (String) dep.get("depId");
+			System.out.println("depId: " + depId);
 
-				JSONArray employee = (JSONArray) eachDep.get("employee");
-				//Loop employees and get values for each employee
-				for (int e = 0; e < employee.size() ; e++) {
-					System.out.println("\n-------employee-------");
-					JSONObject eachEmp =  (JSONObject) employee.get(e);
+			JSONArray employee = (JSONArray) dep.get("employee");
 
-					String empId = (String) eachEmp.get("empId");
-					System.out.println("empId: " + empId);
+			//Loop employees and get values for each employee
+			for (int e = 0; e < employee.size() ; e++) {
+				System.out.println("\n-------employee-------");
+				JSONObject emp =  (JSONObject) employee.get(e);
 
-					String lastname = (String) eachEmp.get("lastname");
-					System.out.println("lastname: " + lastname);
+				String empId = (String) emp.get("empId");
+				System.out.println("empId: " + empId);
 
-					String firstname = (String) eachEmp.get("firstname");
-					System.out.println("firstname: " + firstname);
+				String lastname = (String) emp.get("lastname");
+				System.out.println("lastname: " + lastname);
 
-					String birthDate = (String) eachEmp.get("birthDate");
-					System.out.println("birthDate: " + birthDate);
+				String firstname = (String) emp.get("firstname");
+				System.out.println("firstname: " + firstname);
 
-					String position = (String) eachEmp.get("position");
-					System.out.println("position: " + position);
+				String birthDate = (String) emp.get("birthDate");
+				System.out.println("birthDate: " + birthDate);
+
+				String position = (String) emp.get("position");
+				System.out.println("position: " + position);
 					
-					// Object "skills" contain "skill" array
-					System.out.println("skills:");
-					JSONObject skills = (JSONObject) eachEmp.get("skills");
-					JSONArray skill = (JSONArray) skills.get("skill");
-					// Create iterator for reading skill's value
-					@SuppressWarnings("unchecked")
-					Iterator<String> iterator = skill.iterator();
-					while (iterator.hasNext()) {
-						System.out.println("skill: " + iterator.next());
-					}
-					String managerId = (String) eachEmp.get("managerId");
-					System.out.println("managerId: " + managerId);
-				}//for employee
-			}//for department
-		}//for company
+				// Object "skills" contain "skill" array
+				System.out.println("skills:");
+				JSONObject skills = (JSONObject) emp.get("skills");
+				JSONArray skill = (JSONArray) skills.get("skill");
+				// Create iterator for reading skill's value
+				@SuppressWarnings("unchecked")
+				Iterator<String> iterator = skill.iterator();
+				while (iterator.hasNext()) {
+					System.out.println("skill: " + iterator.next());
+				}
+				String managerId = (String) emp.get("managerId");
+				System.out.println("managerId: " + managerId);
+			}//for employee
+		}//for department
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e1) {
